d4c9ca7de877caffd304700acfb92df2007401f5
Shawn Cicoria <github@cicoria.com>
2024-04-09 20:06:12 -0400

    testing ci

--- asa-helper-action/Program.cs
+++ asa-helper-action/Program.cs
@@ -5,6 +5,7 @@ namespace AsaHelperAction
     {
         public static void Main(string[] args)
         {
+            var aw = new ActionWriter();
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.Configure<StreamingJobOptions>(builder.Configuration.GetSection("jobOptions"));
             builder.Configuration.AddCommandLine(args);
@@ -12,7 +13,29 @@ namespace AsaHelperAction
 
             var host = builder.Build();
             host.Run();
+            Console.WriteLine("::notice msg=done");
+            aw.Write("Action completed");
         }
 
+    }
+
+    public class ActionWriter
+    {
+        StreamWriter? _streamWriter;
+
+        public ActionWriter()
+        {
+            string? logFilePath = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+            if (logFilePath != null)
+            {
+                var fileStream = new FileStream(logFilePath, FileMode.Create, FileAccess.Write, FileShare.Read);
+                _streamWriter = new StreamWriter(fileStream) { AutoFlush = true };
+            }
+
+        }
+        public void Write(string message)
+        {
+            _streamWriter?.WriteLine($"result={message}");
+        }
     }
 }
